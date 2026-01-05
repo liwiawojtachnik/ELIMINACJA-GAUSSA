075b221608bef644588f90dc35a123d4fee80e9d
karolinakosc <01206779@pw.edu.pl>
2026-01-05 15:29:34 +0100

    Karolina : dodanie pivotingu do eliminacji Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,32 @@
 #include "gauss.h"
 #include <math.h>
+
 /**
- * Zwraca 0 - elimnacja zakonczona sukcesem
+ * Zwraca 0 - eliminacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
     int n = mat->r;
+
     for(int k = 0; k < n - 1; k++) {
+        
+        int kmax = k;
+        for (int i = k + 1; i < n; i++) 
+            if (fabs(mat->data[i][k]) > fabs(mat->data[kmax][k])) 
+                kmax = i;
+        
+        if (kmax != k) {
+            double *tmp = mat->data[k];
+            mat->data[k] = mat->data[kmax];
+            mat->data[kmax] = tmp;
+
+            double *tmp_b = b->data[k];
+            b->data[k] = b->data[kmax];
+            b->data[kmax] = tmp_b;
+        }
+
         for(int i = k + 1; i < n; i++) {
-            if (fabs(mat->data[k][k]) < 1e-12) return 1; 
+            if (fabs(mat->data[k][k]) < 1e-12) return 1;
 
             double factor = mat->data[i][k] / mat->data[k][k];
 
@@ -18,7 +36,8 @@ int eliminate(Matrix *mat, Matrix *b){
             b->data[i][0] -= factor * b->data[k][0];
         }
     }
+    
     if (fabs(mat->data[n-1][n-1]) < 1e-12) return 1;
-		return 0;
-}
 
+    return 0;
+}
