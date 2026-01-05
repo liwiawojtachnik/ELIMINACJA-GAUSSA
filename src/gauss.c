34cd99f96823d42f962074d4c34524772f556c47
liwiawojtachnik <01206838@pw.edu.pl>
2026-01-05 15:29:47 +0100

    Liwia: Implementacja wyboru elementu glownego

--- src/gauss.c
+++ src/gauss.c
@@ -7,9 +7,26 @@
 int eliminate(Matrix *mat, Matrix *b){
     int n = mat->r;
     for(int k = 0; k < n - 1; k++) {
-        for(int i = k + 1; i < n; i++) {
-            if (fabs(mat->data[k][k]) < 1e-12) return 1; 
 
+        int max_row = k;
+        for(int i = k + 1; i < n; i++) {
+            if (fabs(mat->data[i][k]) > fabs(mat->data[max_row][k])){ 
+                max_row = i;
+        }
+    }
+
+        if (max_row != k){
+            double *temp = mat->data[k];
+            mat->data[k] = mat->data[max_row];
+            mat->data[max_row] = temp;
+
+            double *temp_b = b->data[k];
+            b->data[k] = b->data[max_row];
+            b->data[max_row] = temp_b;
+        }
+        for(int i = k + 1; i < n; i++) {
+            if (fabs(mat->data[k][k]) < 1e-12) return 1;
+            
             double factor = mat->data[i][k] / mat->data[k][k];
 
             for(int j = k; j < n; j++) {
@@ -18,7 +35,9 @@ int eliminate(Matrix *mat, Matrix *b){
             b->data[i][0] -= factor * b->data[k][0];
         }
     }
+
     if (fabs(mat->data[n-1][n-1]) < 1e-12) return 1;
-		return 0;
+
+	return 0;
 }
 
